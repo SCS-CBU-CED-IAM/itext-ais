bd86aad1a20547e516dd600a06a9217282376398
Miroslav Batchkarov <mbatchkarov@gmail.com>
2019-07-15 11:59:26 +0100

    trigger ci

--- src/test/java/co/teebly/signature/TestSign.java
+++ src/test/java/co/teebly/signature/TestSign.java
@@ -15,7 +15,7 @@ public class TestSign {
         FileReference ref = FileReference.createFileReference(new URI(path));
 
         SignatureRequest sr = new SignatureRequest(
-                "sig.request†.1234", "Max Musterman", "Maximilian Rudolph",
+                "sig.request†.12345", "Max Musterman", "Maximilian Rudolph",
                 "Musterman", "en", "447708216475", "GB",
                 "miro@teebly.co", ref);
 
