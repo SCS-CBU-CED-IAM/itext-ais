7fcadc6278c0a505e504e133c15d80e01ec9d7cd
teebly <teebly@localhost>
2019-07-17 08:45:05 +0200

    Updated TestSign:
    - Added fileReferenceSigned

--- src/test/java/co/teebly/signature/TestSign.java
+++ src/test/java/co/teebly/signature/TestSign.java
@@ -35,7 +35,7 @@ public class TestSign {
         SignatureRequest sr = new SignatureRequest(
                 "sig.request.54321", "Max Musterman", "Maximilian Rudolph",
                 "Musterman", "en", "447708216475", "GB",
-                "miro@teebly.co", getRef("teebly.pdf"), getRef("test.png"),
+                "miro@teebly.co", getRef("teebly.pdf"), null, getRef("test.png"),
                 false);
 
         SqsMessageProducer.process(sr);
