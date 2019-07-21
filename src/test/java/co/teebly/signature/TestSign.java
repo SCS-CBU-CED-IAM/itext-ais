31d6ec7aa8820f367706f6ffccaf912046b0f001
teebly <teebly@localhost>
2019-07-21 10:52:57 +0200

    Updated TestSign:
    - Added support for SignatureRequest.docId

--- src/test/java/co/teebly/signature/TestSign.java
+++ src/test/java/co/teebly/signature/TestSign.java
@@ -33,7 +33,7 @@ public class TestSign {
     @Test
     public void test_org_signature() throws URISyntaxException, IOException {
         SignatureRequest sr = new SignatureRequest(
-                "sig.request.54321", "Max Musterman", "Maximilian Rudolph",
+                "sig.request.54321", "docId", "Max Musterman", "Maximilian Rudolph",
                 "Musterman", "en", "447708216475", "GB",
                 "miro@teebly.co", getRef("teebly.pdf"), null, getRef("test.png"),
                 "theUserId", false);
